05962347e3c741d71c214fc05e5308772be46d5e
martinfrancois <f.martin@fastmail.com>
2020-03-03 23:23:02 +0100

    build(npm): add prettier tools to remove conflicting eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        project: './tsconfig.json',
-        tsconfigRootDir: __dirname,
-    },
-    "plugins": ["@typescript-eslint", "jest"],
-    "extends": [
-        "eslint:recommended",
-        "plugin:jest/recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-    ],
-    "rules": {
-    },
-}
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  extends: [
+    'eslint:recommended',
+    'plugin:jest/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended' // must be last to override conflicting rules!
+  ],
+  rules: {}
+};
