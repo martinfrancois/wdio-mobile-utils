1103587c36174903c4c683d5335d32db81ee4fbf
martinfrancois <f.martin@fastmail.com>
2020-05-07 15:09:28 +0200

    ci: enable travis to push to github

--- .github/deploy_prepare.sh
+++ .github/deploy_prepare.sh
@@ -1,2 +1,7 @@
+echo "Prepare Git for Commits"
 git config user.name "Travis CI"
 git config user.email "build@travis-ci.com"
+git remote add upstream "https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
+git fetch upstream
+git checkout $TAG_BRANCH
+echo "Finished Prepare Git for Commits"
