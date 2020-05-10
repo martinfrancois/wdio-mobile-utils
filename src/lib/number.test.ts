74cc82085a081069c702e651849d5c736e717021
martinfrancois <f.martin@fastmail.com>
2020-05-10 13:05:19 +0200

    chore: remove redundant code
    Signed-off-by: martinfrancois <f.martin@fastmail.com>

--- src/lib/number.test.ts
+++
@@ -1,9 +0,0 @@
-import { double, power } from './number';
-
-it('should double the value', () => {
-    expect(double(2)).toBe(4);
-});
-
-it('should calculate the fourth power of two', () => {
-    expect(power(2, 4)).toBe(16);
-});
